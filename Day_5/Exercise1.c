a726f8408fb98ef65cff75c6543fe437dc9cbc70
Ernafandeu <johannerna0@gmail.com>
2022-09-05 09:30:57 +0200

    The fith day of learning C langage

---
+++ Day_5/Exercise1.c
@@ -0,0 +1,12 @@
+/* Write a header for a function named do_it() that takes three type char arguments and returns a
+type float to the calling program. */
+
+#include <stdio.h>
+int main ()
+{
+
+    return 0;
+}
+float do_it(char a, char b, char c) {
+    return a;
+}
